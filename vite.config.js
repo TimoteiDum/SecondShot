84f3afa4279e3ffe0c363a22c71a1f6f60a3579f
TimoteiDum <dumbraveanut@example.com>
2026-01-19 13:15:53 +0200

    probaa

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
   base: '/SecondShot/',
   plugins: [
     vue(),
-    vueDevTools(),
-    tailwindcss()
+    vueDevTools()
   ],
   resolve: {
     alias: {
